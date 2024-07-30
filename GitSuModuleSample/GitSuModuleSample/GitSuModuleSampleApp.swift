ba9629cf77201d77537f28693b960d26e5750c2b
Fouad Anwar <fauad.anwar@gmail.com>
2024-07-30 16:39:55 +0530

    Created Git SubModule Sample

---
+++ GitSuModuleSample/GitSuModuleSample/GitSuModuleSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSuModuleSampleApp.swift
+//  GitSuModuleSample
+//
+//  Created by Fouad Mohammed Rafique Anwar on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GitSuModuleSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
