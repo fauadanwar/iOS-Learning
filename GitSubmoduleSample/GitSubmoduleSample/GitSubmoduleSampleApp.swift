8fa73e67c5595db1c425dccb3ab64edae3085d71
Fouad Anwar <fauad.anwar@gmail.com>
2024-07-30 18:13:45 +0530

    GitSubmoduleSample App now loaded with FZImageCache.git Framework

---
+++ GitSubmoduleSample/GitSubmoduleSample/GitSubmoduleSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSubmoduleSampleApp.swift
+//  GitSubmoduleSample
+//
+//  Created by Fouad Mohammed Rafique Anwar on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GitSubmoduleSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
